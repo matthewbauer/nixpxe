1dc054ad63db91a77cc4c0b74f18bfc5c09ca347
Matthew Bauer <mjbauer95@gmail.com>
2020-10-02 00:06:19 -0400

    Set timeZone

--- system.nix
+++ system.nix
@@ -13,10 +13,11 @@ in import (pkgs.path + /nixos/lib/eval-config.nix) {
       networking.hostName =
         if macAddress == "*" then hostName
         else config'.hostName;
-      i18n.defaultLocale = "${config'.lang}.UTF-8";
-      i18n.supportedLocales = [ "${config'.lang}.UTF-8/UTF-8" ];
-      users.users.builder.openssh.authorizedKeys.keys = config'.authorizedKeys;
-      users.users.root.openssh.authorizedKeys.keys = config'.rootAuthorizedKeys;
+      time.timeZone = lib.mkIf (config' ? timeZone) config'.timeZone;
+      i18n.defaultLocale = lib.mkIf (config' ? lang) "${config'.lang}.UTF-8";
+      i18n.supportedLocales = lib.mkIf (config' ? lang) [ "${config'.lang}.UTF-8/UTF-8" ];
+      users.users.builder.openssh.authorizedKeys.keys = lib.mkIf (config' ? authorizedKeys) config'.authorizedKeys;
+      users.users.root.openssh.authorizedKeys.keys = lib.mkIf (config' ? rootAuthorizedKeys) config'.rootAuthorizedKeys;
 
       nixpkgs.crossSystem = lib.mkIf (config' ? system && config'.system != system) { inherit (config') system; };
       nix.maxJobs = lib.mkIf (config' ? maxJobs) config'.maxJobs;
