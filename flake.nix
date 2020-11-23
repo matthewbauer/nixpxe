c9cbbc84770355f715069eecf1029ade3c7497e0
Matthew Bauer <mjbauer95@gmail.com>
2020-11-23 04:07:09 +0000

    Remove nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   description = "Nixpxe";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   inputs.netbootpkgs.url = "github:matthewbauer/netboot.nix?ref=flakes";
 
-  outputs = { self, nixpkgs, netbootpkgs }: {
+  outputs = { self, netbootpkgs }: {
     nixosModule = import ./nixos.nix { netbootpkgsSrc = netbootpkgs; };
     defaultTemplate.path = ./template;
     defaultTemplate.description = "Default template";
